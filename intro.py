1deaa595e7ae32798dfc687f8edd6709d942b86e
Sandeep M Waghmare <sandeepwaghmare1999@gmail.com>
2022-07-23 12:12:54 +0530

    Introduction

---
+++ intro.py
@@ -0,0 +1,15 @@
+'''
+Github -----> Github Desktop
+
+Linkdin Profile
+Inftq website
+
+Basics + Functions + OOPs + DSA + DBMS
+
+Interview :- Sat
+A :- Concept  - > 10 ques + 5-7 homework
+test -> 4
+
+
+B:- Content - Content
+'''
